308b3262a82a4c15e4f43413efe1fd52b5bc3880
Datumza <reitumetsemasilo@gmail.com>
2025-01-02 15:19:40 +0200

    refactor: Correct path

--- tests/test_macd.py
+++ tests/test_macd.py
@@ -1,7 +1,11 @@
-import pytest  # pylint: disable=unused-import
+import os
 import pandas as pd
+import pytest  # pylint: disable=unused-import
 from contigion_indicators.macd import macd_crossover
 from contigion_indicators.util.functions import get_dataframe_size
+
+current_dir = os.path.dirname(__file__)
+csv_path = os.path.join(current_dir, "resources", "data.csv")
 
 
 def test_maccd_crossover():
@@ -10,7 +14,7 @@ def test_maccd_crossover():
     slow = 26
     signal = 9
 
-    data = pd.read_csv('resources/data.csv')
+    data = pd.read_csv(csv_path)
     macd_data = macd_crossover(data, fast, slow, signal).drop(columns=['signal']).dropna(inplace=False)
 
     assert (get_dataframe_size(macd_data) == (n_candles - slow - signal + 1))
--- tests/test_rsi.py
+++ tests/test_rsi.py
@@ -1,14 +1,18 @@
-import pytest  # pylint: disable=unused-import
+import os
 import pandas as pd
+import pytest  # pylint: disable=unused-import
 from contigion_indicators.rsi import rsi, rsi_mavg, rsi_over_bought_sold
 from contigion_indicators.util.functions import get_dataframe_size
+
+current_dir = os.path.dirname(__file__)
+csv_path = os.path.join(current_dir, "resources", "data.csv")
 
 
 def test_rsi():
     n_candles = 500
     period = 7
 
-    data = pd.read_csv('resources/data.csv')
+    data = pd.read_csv(csv_path)
     rsi_data = rsi(data, period).drop(columns=['signal']).dropna(inplace=False)
 
     assert (get_dataframe_size(rsi_data) == (n_candles - period - 1))
@@ -19,7 +23,7 @@ def test_rsi_mavg():
     period = 7
     mavg = 14
 
-    data = pd.read_csv('resources/data.csv')
+    data = pd.read_csv(csv_path)
     rsi_data = rsi_mavg(data, period, mavg).drop(columns=['signal']).dropna(inplace=False)
 
     assert (get_dataframe_size(rsi_data) == (n_candles - mavg - period))
@@ -29,7 +33,7 @@ def test_rsi_over_bought_sold():
     n_candles = 500 - 1
     period = 7
 
-    data = pd.read_csv('resources/data.csv')
+    data = pd.read_csv(csv_path)
     rsi_data = rsi_over_bought_sold(data, period).drop(columns=['signal']).dropna(inplace=False)
 
     assert (get_dataframe_size(rsi_data) == (n_candles - period - 1))
